090becf3b1c1b20a6d71dff9608e4418b12a5f54
Anthony Villano <villaa@physics.umn.edu>
2017-02-18 19:19:22 -0600

    [ANV] added some functions for reading primaries files in text

---
+++ root/readPhotoNSuperSimTxt.C
@@ -0,0 +1,89 @@
+
+
+#include <iomanip>
+TTree *readPhotoNSuperSimTxtPrimaries(int &nev,int n=-1,int datasetno=0,string source="88y",string dir="/data/chocula/villaa/PhotoN_SuperSim/88y",string treename="mcprimaries")
+{
+   //get data set number string in hex
+   string dataset="";
+   std::ostringstream oss;
+   oss << std::hex << "0x" << std::setfill('0') << std::setw(4) << datasetno;
+   dataset = oss.str(); 
+
+  //create a chain
+  TTree *data = new TTree(treename.c_str(),treename.c_str());
+
+  //set up the variables
+  double E,x,y,z,px,py,pz,D,t,shift;
+  int event,type;
+  data->Branch("E",&E,"E/D");
+  data->Branch("x",&x,"x/D");
+  data->Branch("y",&y,"y/D");
+  data->Branch("z",&z,"z/D");
+  data->Branch("px",&px,"px/D");
+  data->Branch("py",&py,"py/D");
+  data->Branch("pz",&pz,"pz/D");
+  data->Branch("D",&D,"D/D");
+  data->Branch("t",&t,"t/D");
+  data->Branch("shift",&shift,"shift/D");
+  data->Branch("event",&event,"event/I");
+  data->Branch("type",&type,"type/I");
+
+  //make a command to use with a root pipe
+  command = "ls "+dir+" |grep Primary.txt |grep "+dataset+"_ |grep "+source;
+  TString files = gSystem->GetFromPipe(command.c_str());
+  istringstream iss(files);
+
+  //get the files from the string
+  string filename;
+
+  //make a counter
+  int count=0;
+
+  while(!iss.eof()){
+    iss >> filename;
+    cout << filename << endl;
+
+    //extract the shift from the filename
+    int p0 = filename.find("shift");
+    int p1 = filename.find("_p10000");
+    string strshift = filename.substr(p0+5,p1-p0-5);
+    //strshift = "134_0556";
+    cout << strshift << endl;
+    int u = strshift.find("_");
+    string strshift_dec;
+    if(u!=string::npos){
+      strshift_dec=strshift.substr(0,u);
+      strshift_dec+=".";
+      strshift_dec+=strshift.substr(u+1,strshift.size()-u);
+    }
+    else
+      strshift_dec=strshift;
+
+    //change to double
+    istringstream streamshift(strshift_dec);
+    streamshift >> shift;
+
+    //fill the tree
+    ifstream streamfile((dir+"/"+filename).c_str(),ios::in);
+    cout << (dir+"/"+filename) << endl;
+    int filecount=0;
+    while(!streamfile.eof()){
+      if(filecount>2){
+        streamfile >> event >> type >> E >> px >> py >> pz >> x >> y >> z >> D >> t;
+	data->Fill();
+	//cout << filecount << "\t" << event << "\t" << type << "\t" << E << "\t" << px << "\t" << py << "\t" << pz << "\t" << x << "\t" << y << "\t" << z << "\t" << t << endl;
+      }
+      else{
+	string junk;
+	getline(streamfile,junk);
+      }
+      filecount++;
+    }
+    streamfile.close();
+
+    count++;
+  }
+  
+  return data;
+
+}
