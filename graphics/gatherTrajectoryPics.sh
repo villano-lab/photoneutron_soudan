45094978a6535ad13769b2e6ffe5e0e4bc6a1359
Anthony Villano <villaa@physics.umn.edu>
2018-02-06 16:46:31 -0600

    [ANV] update commit

--- graphics/gatherTrajectoryPics.sh
+++ graphics/gatherTrajectoryPics.sh
@@ -9,12 +9,12 @@ PICSUFFIX=`date +%y%m%d"_"%H%M%S"_"%N`
 echo ${SUFFIX}
 echo ${PICSUFFIX}
 
-TEMP=`cat trajectorySupersim_${SUFFIX}.txt |grep 5|awk 'END{print NR;}'`
+TEMP=`cat trajectorySupersim_${SUFFIX}.txt |grep -v 'Run\|D3'|awk 'END{print NR-1;}'`
 echo ${TEMP}
 
 if [ ${TEMP} != "0" ]
 then
-  mv trajectory-shift_6_7_0.eps trajectory_pics/trajectory-shift_6_7_${PICSUFFIX}.eps
+  mv trajectory-shift_6_7_0.eps trajectory_pics/trajectory-shift_6_7_${PICSUFFIX}_100Tracks.eps
 fi
 
 rm trajectorySupersim_${SUFFIX}.txt
