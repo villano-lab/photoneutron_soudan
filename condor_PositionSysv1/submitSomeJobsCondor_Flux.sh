36ef314f5e8392852dbfa145a79a3f90f591cf76
Anthony Villano <villaa@physics.umn.edu>
2018-03-02 19:44:16 -0600

    [ANV] added some code to submit a flux run for sbbe source

--- condor_PositionSysv1/submitSomeJobsCondor_Flux.sh
+++ condor_PositionSysv1/submitSomeJobsCondor_Flux.sh
@@ -2,6 +2,5 @@
 
 
 #3/2/18
-#do just the along bridge shift with updated code develop tag elog/445
-# loop to submit many jobs 
-./condorPhotoNSuperSim_PositionSys -n 1 -src 88y -set 10 -ngen 3700000 -prim -otype root -template fluxcount_PNZip.mac.template -shift 0 -srcZ 39 -srcA 88 -exe
+# submit a flux job for the zips 
+./condorPhotoNSuperSim_PositionSys -n 100 -src 88y -set 10 -ngen 3700000 -prim -otype root -template fluxcount_PNZip.mac.template -shift 0 -srcZ 39 -srcA 88 -exe
